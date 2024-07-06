086bdb3334b84d8acea01fff2ab7c99323249e20
jiwoo <zbqmgldjfh@gmail.com>
2024-07-06 21:20:17 +0900

    feet(UserSerializer): UserSerializer에 products 연관관계 추가

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -11,9 +11,10 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
 
-    json_response = JSON.parse(self.response.body)
-    print json_response
-    assert_equal @user.email, json_response['data'][ 'attributes']['email']
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal @user.email, json_response.dig(:data, :attributes, :email)
+    assert_equal @user.products.first.id.to_s, json_response. dig(:data, :relationships, :products, :data, 0, :id)
+    assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
   end
 
   test "사용자를 신규로 생성할 수 있다" do
