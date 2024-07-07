328495c56566980754c77c13096f4d83edf7be2d
jiwoo <zbqmgldjfh@gmail.com>
2024-07-07 15:06:58 +0900

    feet(OrdersController#show): 단일 주문 확인하는 api 구현

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::OrdersController < ApplicationController
-  before_action :check_login, only: [:index]
+  before_action :check_login, only: %i[index show]
 
   def index
     render json: OrderSerializer.new(current_user.orders).serializable_hash
+  end
+
+  def show
+    find_order = current_user.orders.find([params[:id]])
+
+    if find_order
+      options = { include: [:products] }
+      return render json: OrderSerializer.new(find_order, options).serializable_hash
+    end
+
+    head 404
   end
 
   private def check_login
