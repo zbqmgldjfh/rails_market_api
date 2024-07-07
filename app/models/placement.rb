ecccf806cd74f3b40cbe3af79aaa4b409d497ba1
jiwoo <zbqmgldjfh@gmail.com>
2024-07-07 20:01:36 +0900

    feat(Placement): Product의 제고 감소로직 추가

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,6 +1,11 @@
 class Placement < ApplicationRecord
-  belongs_to :order
+  after_create :decrement_product_quantity!
 
+  belongs_to :order
   # inverse_of가 선언되면 업데이트 문을 명시적으로 작성하지 않고도 관련 모델을 업데이트할 수 있습니다.
   belongs_to :product, inverse_of: :placements
+
+  def decrement_product_quantity!
+    product.decrement(:quantity, quantity)
+  end
 end
